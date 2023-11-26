d657d1e236dfb6c5a88ea2a933cdb6421d865be9
Sam Churchill <meadowschurchill@gmail.com>
2023-11-26 15:23:07 -0800

    return null/empty

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -3,5 +3,17 @@ import { authenticate } from "../shopify.server"
 export const loader = async ({ request }) => {
     const { redirect } = await authenticate.admin(request)
     const url = new URL(request.url);
-    throw redirect(`/app?${url.searchParams.toString()}`);
+    if (url.searchParams.get("shop")) {
+        throw redirect(`/app?${url.searchParams.toString()}`);
+    }
+
+    return null
+}
+
+export default function empty() {
+    return (
+        <div>
+            empty
+        </div>
+    )
 }
