b5c75ededb6b21833e6513d51c3b48e136a90e7d
Sam Churchill <meadowschurchill@gmail.com>
2023-11-27 20:13:43 -0800

    might need these boundaries

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,6 +1,14 @@
 import { authenticate } from "../shopify.server"
+import { boundary } from '@shopify/shopify-app-remix/server';
 
 export const loader = async ({ request }) => {
     const { redirect } = await authenticate.admin(request)
     throw redirect("/app/")
 }
+
+export function ErrorBoundary() {
+    return boundary.error(useRouteError());
+}
+export const headers = (headersArgs) => {
+    return boundary.headers(headersArgs);
+};
