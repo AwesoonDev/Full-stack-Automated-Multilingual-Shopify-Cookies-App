c13e783b8a999a93b4035afee99d933a6c7c01e0
Sam Churchill <meadowschurchill@gmail.com>
2023-11-27 20:30:03 -0800

    naive routing does not work but does it load?

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,15 +1,41 @@
-import { useRouteError } from "@remix-run/react";
-import { authenticate } from "../shopify.server"
-import { boundary } from '@shopify/shopify-app-remix/server';
+import { json } from "@remix-run/node";
+import { Link, Outlet, useLoaderData, useRouteError } from "@remix-run/react";
+import polarisStyles from "@shopify/polaris/build/esm/styles.css";
+import { boundary } from "@shopify/shopify-app-remix/server";
+import { AppProvider } from "@shopify/shopify-app-remix/react";
+import { authenticate } from "../shopify.server";
+import { Trans } from "react-i18next";
+
+export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
 
 export const loader = async ({ request }) => {
-    const { redirect } = await authenticate.admin(request)
-    throw redirect("/app/")
+  await authenticate.admin(request);
+  return json({ apiKey: process.env.SHOPIFY_API_KEY || "" });
+};
+
+export default function App() {
+  const { apiKey } = useLoaderData();
+
+  return (
+    <AppProvider isEmbeddedApp apiKey={apiKey}>
+      <ui-nav-menu>
+        <Link to="/home">
+          Home
+        </Link>
+        <Link to="/billing">
+          <Trans i18nKey={"billingTitle"} />
+        </Link>
+      </ui-nav-menu>
+      <Outlet />
+    </AppProvider>
+  );
 }
 
+// Shopify needs Remix to catch some thrown responses, so that their headers are included in the response.
 export function ErrorBoundary() {
-    return boundary.error(useRouteError());
+  return boundary.error(useRouteError());
 }
+
 export const headers = (headersArgs) => {
-    return boundary.headers(headersArgs);
+  return boundary.headers(headersArgs);
 };
